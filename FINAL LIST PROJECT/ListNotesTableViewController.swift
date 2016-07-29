96e6783d5cbf39d468194ac4d760e9c8a7e93f76
Himanshu Bhadoria <himan2016@gmail.com>
2016-07-29 09:12:26 -0700

    Init

---
+++ FINAL LIST PROJECT/ListNotesTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ListNotesTableViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+    // 1
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // 2
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 3
+        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath)
+        
+        // 4
+        cell.textLabel?.text = "Yay - it's working!"
+        
+        // 5
+        return cell
+    }
+  
+}
---
+++ FINAL LIST PROJECT/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FINAL LIST PROJECT
+//
+//  Created by Himanshu Bhadoria on 7/19/16.
+//  Copyright © 2016 HBhadoria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
